0a7f3edfb40f5a2c95fdf25ecf62c77a55b2713d
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-11-18 14:13:25 +0100

    Clase SelectOptionAuthActivity.kt creada para iniciar el registro.

---
+++ app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
@@ -0,0 +1,11 @@
+package com.geohealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectOptionAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_option_auth)
+    }
+}
