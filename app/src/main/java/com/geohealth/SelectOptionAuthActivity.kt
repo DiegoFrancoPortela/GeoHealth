9819a2de44aedf695e6e344720cd2728cbc257a5
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-11-22 10:37:14 +0100

    Añadida una Toolbox para volver a la anterior activity

--- app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
+++ app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
@@ -2,10 +2,18 @@ package com.geohealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class SelectOptionAuthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_option_auth)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.setTitle("Seleccionar Opción")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
