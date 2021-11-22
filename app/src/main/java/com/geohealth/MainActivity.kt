934f27b6db312f9a6a9cab665d6421b955be9093
Alex <afontanmoreira@danielcastelao.org>
2021-11-22 10:24:48 +0100

    Clicklistener y navegar por pantallas

--- app/src/main/java/com/geohealth/MainActivity.kt
+++ app/src/main/java/com/geohealth/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.geohealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mButtonMedic: Button = findViewById(R.id.btnMedic)
+        val mButtonPacient: Button = findViewById(R.id.btnClient)
+        fun goToSelectAuth() {
+            val intent = Intent(this,SelectOptionAuthActivity::class.java)
+            startActivity(intent)
+        }
+        mButtonPacient.setOnClickListener(){
+            goToSelectAuth()
+        }
+        mButtonMedic.setOnClickListener(){
+            goToSelectAuth()
+        }
+
+
     }
 }
