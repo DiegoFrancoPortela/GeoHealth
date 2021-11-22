7fb42377ab346336ca47c8d8931e37fd4d750e81
Alex <afontanmoreira@danielcastelao.org>
2021-11-22 11:08:15 +0100

    Creada Toolbar y añadido un nuevo estilo

--- app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
+++ app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
@@ -2,10 +2,14 @@ package com.geohealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class SelectOptionAuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_option_auth)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setTitle("Seleccionar Opcion")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
