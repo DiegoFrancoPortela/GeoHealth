68a5c287685238075f351a6bdbee3e72ec7a68dc
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-11-22 09:55:03 +0100

    Correccion Colores

--- app/src/main/java/com/geohealth/MainActivity.kt
+++ app/src/main/java/com/geohealth/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.geohealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mButtonSoyPaciente: Button = findViewById(R.id.btnSoyPaciente)
+        val mButtonSoyEnfermero: Button = findViewById(R.id.btnSoyEnfermero)
+
+        mButtonSoyPaciente.setOnClickListener() {
+            goToSelectAuth()
+        }
+
+        mButtonSoyEnfermero.setOnClickListener() {
+            goToSelectAuth()
+        }
+
+    }
+
+    private fun goToSelectAuth() {
+        val intent = Intent(this, SelectOptionAuthActivity::class.java)
+        startActivity(intent)
     }
 }
