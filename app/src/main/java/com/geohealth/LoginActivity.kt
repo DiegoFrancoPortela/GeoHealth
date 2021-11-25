016193812d622927cba9f97eca7cf13bf9ec57ca
Alex <afontanmoreira@danielcastelao.org>
2021-11-25 13:45:35 +0100

    Login y Conexion a Firebase

--- app/src/main/java/com/geohealth/LoginActivity.kt
+++ app/src/main/java/com/geohealth/LoginActivity.kt
@@ -2,10 +2,54 @@ package com.geohealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val mTextInputEmail: TextInputEditText = findViewById(R.id.textInputEmail)
+        val mTextInputPassword: TextInputEditText = findViewById(R.id.textInputPassword)
+
+        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        val mDatabase: DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
+        val mButtonLogin: Button = findViewById(R.id.btnLogin)
+
+        fun login() {
+            var email: String = mTextInputEmail.getText().toString()
+            var password: String = mTextInputPassword.getText().toString()
+
+            if (!email.isEmpty() && !password.isEmpty()) {
+                // Validación para ver si la contraseña en mayor o igual a 6
+                if (password.length >= 6) {
+
+                    mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful()) {
+                                Toast.makeText(this,"Login Exitoso",Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this,"Email o Contraseña incorrectos",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+                else {
+                    Toast.makeText(this,"La contraseña debe tener más de 6 carácteres",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this,"La contraseña y el Email son obligatorios",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        mButtonLogin.setOnClickListener() {
+            login()
+        }
     }
 }
--- app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
+++ app/src/main/java/com/geohealth/SelectOptionAuthActivity.kt
@@ -1,15 +1,32 @@
 package com.geohealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toolbar
 
 class SelectOptionAuthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_option_auth)
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setTitle("Seleccionar Opcion")
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val mButtonTengoCuenta: Button = findViewById(R.id.botonGoToLogin)
+        val mButtonRegistro: Button = findViewById(R.id.botonGoToRegister)
+
+        mButtonTengoCuenta.setOnClickListener() {
+            goToLogin()
+        }
+
+        mButtonRegistro.setOnClickListener() {
+
+        }
+    }
+    private fun goToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
