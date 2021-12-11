ef1558639cd80a57bdd16e767f6080ce45f39d84
DiegoFrancoPortela <dfrancoportela@danielcastelao.org>
2021-12-11 22:56:36 +0100

    Creado Método de Logout y botón.

--- app/src/main/java/com/geohealth/MapActivity.kt
+++ app/src/main/java/com/geohealth/MapActivity.kt
@@ -1,11 +1,34 @@
 package com.geohealth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mButtonLogout: Button = findViewById(R.id.btnLogout)
+
+        //Firebase
+        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        //
+
+        fun logout() {
+            // Metodo para cerrar sesión y cambiar de actividad.
+            mAuth.signOut()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            // Finish para terminar esta Activity
+            finish()
+        }
+
+        mButtonLogout.setOnClickListener() {
+            logout()
+        }
+
     }
 }
